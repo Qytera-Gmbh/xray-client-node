6b3f7f177a63819a4841c6dee4c9e00422bfda89
Sebastian Vollbrecht <sebastian.vollbrecht@qytera.de>
2025-02-10 20:42:36 +0100

    Add Xray server dataset import endpoint

--- src/client/xray-client-server.ts
+++ src/client/xray-client-server.ts
@@ -1,14 +1,14 @@
 import type { Xray } from "../../index.js";
+import { DatasetServerApi } from "../endpoints/server/dataset/dataset.js";
 import { TestExecutionApi } from "../endpoints/server/testexec/test-execution.js";
 import { TestPlanApi } from "../endpoints/server/testplan/test-plan.js";
 import { ExecutionEvidenceApi } from "../endpoints/server/testrun/attachment/attachment.js";
-import { DatasetApi } from "../endpoints/shared/dataset/dataset.js";
 import { ImportExecutionApi } from "../endpoints/shared/import/execution/import-execution.js";
 import type { ClientConfiguration } from "./base-client.js";
 import { BaseClient } from "./base-client.js";
 
 export class XrayClientServer extends BaseClient {
-  dataset = new DatasetApi<Xray.Dataset.ExportQueryServer>(this);
+  dataset = new DatasetServerApi(this);
   import = {
     execution: new ImportExecutionApi<Xray.Import.ResponseServer>(this),
   };
