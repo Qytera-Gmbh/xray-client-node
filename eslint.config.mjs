56d11a5d2706b41bd07b43799ecce37930346eda
Sebastian Vollbrecht <sebivo@freenet.de>
2025-02-15 01:09:04 +0100

    Make graphql and graphql-tag optional peer dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,86 +5,107 @@ import perfectionist from "eslint-plugin-perfectionist";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-    eslint.configs.recommended,
-    ...tseslint.configs.strictTypeChecked,
-    ...tseslint.configs.stylisticTypeChecked,
-    {
-        languageOptions: {
-            parserOptions: {
-                projectService: true,
-                tsconfigRootDir: import.meta.dirname,
+  eslint.configs.recommended,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: { perfectionist },
+  },
+  {
+    rules: {
+      "@typescript-eslint/consistent-type-exports": "error",
+      "@typescript-eslint/consistent-type-imports": "error",
+      "@typescript-eslint/member-ordering": "error",
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          format: ["strictCamelCase"],
+          selector: "default",
+        },
+        {
+          format: ["UPPER_CASE"],
+          modifiers: ["global", "const"],
+          selector: "variable",
+        },
+        {
+          format: ["UPPER_CASE"],
+          modifiers: ["static", "readonly"],
+          selector: "memberLike",
+        },
+        {
+          format: ["UPPER_CASE"],
+          selector: "enumMember",
+        },
+        {
+          format: ["StrictPascalCase"],
+          selector: "typeLike",
+        },
+        {
+          format: ["StrictPascalCase", "strictCamelCase"],
+          selector: "import",
+        },
+      ],
+      "@typescript-eslint/no-deprecated": "off",
+      "@typescript-eslint/no-shadow": "error",
+      "@typescript-eslint/non-nullable-type-assertion-style": "off",
+      "@typescript-eslint/parameter-properties": "error",
+      "@typescript-eslint/prefer-readonly": "error",
+      "no-shadow": "off",
+      "perfectionist/sort-array-includes": ["error", { order: "asc", type: "alphabetical" }],
+      "perfectionist/sort-classes": [
+        "error",
+        {
+          groups: [
+            "index-signature",
+            "static-property",
+            "static-block",
+            ["property", "accessor-property"],
+            ["protected-property", "protected-accessor-property"],
+            ["private-property", "private-accessor-property"],
+            "constructor",
+            "static-method",
+            ["get-method", "set-method"],
+            "protected-method",
+            "private-method",
+            "method",
+            "unknown",
+          ],
+          order: "asc",
+          type: "alphabetical",
+        },
+      ],
+      "perfectionist/sort-enums": ["error", { order: "asc", type: "alphabetical" }],
+      "perfectionist/sort-exports": ["error", { order: "asc", type: "alphabetical" }],
+      "perfectionist/sort-interfaces": ["error", { order: "asc", type: "alphabetical" }],
+      "perfectionist/sort-named-exports": ["error", { order: "asc", type: "alphabetical" }],
+      "perfectionist/sort-object-types": ["error", { order: "asc", type: "alphabetical" }],
+      "perfectionist/sort-objects": ["error", { order: "asc", type: "alphabetical" }],
+      "perfectionist/sort-union-types": ["error", { order: "asc", type: "alphabetical" }],
+    },
+  },
+  { files: ["**/*.mjs"], ...tseslint.configs.disableTypeChecked },
+  { ignores: ["dist/", "**/*.js"] },
+  {
+    files: ["**/*.spec.ts"],
+    rules: {
+      "@typescript-eslint/no-floating-promises": [
+        "warn",
+        {
+          allowForKnownSafeCalls: [
+            {
+              from: "package",
+              name: ["it", "describe"],
+              package: "node:test",
             },
+          ],
         },
-        plugins: { perfectionist },
+      ],
     },
-    {
-        rules: {
-            "@typescript-eslint/consistent-type-exports": "error",
-            "@typescript-eslint/consistent-type-imports": "error",
-            "@typescript-eslint/member-ordering": "error",
-            "@typescript-eslint/naming-convention": [
-                "error",
-                {
-                    format: ["strictCamelCase"],
-                    selector: "default",
-                },
-                {
-                    format: ["UPPER_CASE"],
-                    modifiers: ["global", "const"],
-                    selector: "variable",
-                },
-                {
-                    format: ["UPPER_CASE"],
-                    modifiers: ["static", "readonly"],
-                    selector: "memberLike",
-                },
-                {
-                    format: ["UPPER_CASE"],
-                    selector: "enumMember",
-                },
-                {
-                    format: ["StrictPascalCase"],
-                    selector: "typeLike",
-                },
-                {
-                    format: ["StrictPascalCase", "strictCamelCase"],
-                    selector: "import",
-                },
-            ],
-            "@typescript-eslint/no-deprecated": "off",
-            "@typescript-eslint/no-shadow": "error",
-            "@typescript-eslint/non-nullable-type-assertion-style": "off",
-            "@typescript-eslint/parameter-properties": "error",
-            "@typescript-eslint/prefer-readonly": "error",
-            "no-shadow": "off",
-            "perfectionist/sort-array-includes": ["error", { order: "asc", type: "alphabetical" }],
-            "perfectionist/sort-classes": ["error", { order: "asc", type: "alphabetical" }],
-            "perfectionist/sort-enums": ["error", { order: "asc", type: "alphabetical" }],
-            "perfectionist/sort-exports": ["error", { order: "asc", type: "alphabetical" }],
-            "perfectionist/sort-interfaces": ["error", { order: "asc", type: "alphabetical" }],
-            "perfectionist/sort-named-exports": ["error", { order: "asc", type: "alphabetical" }],
-            "perfectionist/sort-object-types": ["error", { order: "asc", type: "alphabetical" }],
-            "perfectionist/sort-objects": ["error", { order: "asc", type: "alphabetical" }],
-            "perfectionist/sort-union-types": ["error", { order: "asc", type: "alphabetical" }],
-        },
-    },
-    { files: ["**/*.mjs"], ...tseslint.configs.disableTypeChecked },
-    { ignores: ["dist/", "**/*.js"] },
-    {
-        files: ["**/*.spec.ts"],
-        rules: {
-            "@typescript-eslint/no-floating-promises": [
-                "warn",
-                {
-                    allowForKnownSafeCalls: [
-                        {
-                            from: "package",
-                            name: ["it", "describe"],
-                            package: "node:test",
-                        },
-                    ],
-                },
-            ],
-        },
-    }
+  }
 );
