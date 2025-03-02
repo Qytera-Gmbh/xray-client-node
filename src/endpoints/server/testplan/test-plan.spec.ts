f46a2fc9d29e9dda4a5abd22224cdc956c99f092
Sebastian Vollbrecht <sebivo@freenet.de>
2025-03-02 09:56:34 +0100

    Fix assertions

--- src/endpoints/server/testplan/test-plan.spec.ts
+++ src/endpoints/server/testplan/test-plan.spec.ts
@@ -11,8 +11,8 @@ describe(path.relative(process.cwd(), import.meta.filename), () => {
         DATA_SERVER.testPlans.immutable.key
       );
       assert.strictEqual(content.length, 2);
-      assert.strictEqual(content[0].key, DATA_SERVER.testPlans.immutable.testExecutions[0]);
-      assert.strictEqual(content[1].key, DATA_SERVER.testPlans.immutable.testExecutions[1]);
+      assert.strictEqual(content[0].key, DATA_SERVER.testPlans.immutable.testExecutions[0].key);
+      assert.strictEqual(content[1].key, DATA_SERVER.testPlans.immutable.testExecutions[1].key);
     });
   });
 
@@ -22,7 +22,7 @@ describe(path.relative(process.cwd(), import.meta.filename), () => {
         DATA_SERVER.testPlans.immutable.key
       );
       assert.strictEqual(content.length, 1);
-      assert.strictEqual(content[0].key, DATA_SERVER.testPlans.immutable.tests[0]);
+      assert.strictEqual(content[0].key, DATA_SERVER.testPlans.immutable.tests[0].key);
     });
   });
 });
