089a1b2a1635550ad0b05bf96a7d965b039ec134
Sebastian Vollbrecht <sebivo@freenet.de>
2025-03-02 16:04:59 +0100

    Improve version handling in attachments endpoint

--- src/endpoints/base-api.ts
+++ src/endpoints/base-api.ts
@@ -14,4 +14,37 @@ export class BaseApi {
   constructor(client: BaseClient) {
     this.client = client;
   }
+
+  /**
+   * Provides a callback with a reference to the calling instance, ensuring that the callback always has access to the correct `this` context.
+   *
+   * @param callback a function that takes the calling instance as an argument
+   * @returns the result of the callback
+   *
+   * @example
+   *
+   * ```ts
+   * class MyApi extends BaseApi {
+   *   private value = "Hello";
+   *   public a = {
+   *     getValue() {
+   *       return this.value;
+   *     },
+   *   };
+   *   public b = this.bind((self) => ({
+   *     getValue() {
+   *       return self.value;
+   *     },
+   *   }));
+   * }
+   * const instance = new MyApi();
+   * const a = instance.a;
+   * console.log(a.getValue()); // undefined
+   * const b = instance.b;
+   * console.log(b.getValue()); // "Hello"
+   * ```
+   */
+  protected bind<T>(callback: (self: this) => T): T {
+    return callback(this);
+  }
 }
--- src/endpoints/cloud/attachments/attachments.spec.ts
+++ src/endpoints/cloud/attachments/attachments.spec.ts
@@ -7,12 +7,14 @@ import { DATA_CLOUD } from "../../../../test/test-data-cloud.js";
 describe(path.relative(process.cwd(), import.meta.filename), () => {
   describe("get attachment", () => {
     for (const [version, endpoint] of [
-      ["v1", XRAY_CLIENT_CLOUD.attachment.getAttachment.v1],
-      ["v2", XRAY_CLIENT_CLOUD.attachment.getAttachment],
+      ["v1", XRAY_CLIENT_CLOUD.attachment.v1],
+      ["v2", XRAY_CLIENT_CLOUD.attachment],
     ] as const) {
       describe(version, () => {
         it(`returns attachment content (${version})`, async () => {
-          const content = await endpoint(DATA_CLOUD.testExecutions.immutable.evidence.id);
+          const content = await endpoint.addAttachment(
+            DATA_CLOUD.testExecutions.immutable.evidence.id
+          );
           assert.strictEqual(content, DATA_CLOUD.testExecutions.immutable.evidence.content);
         });
       });
@@ -20,12 +22,13 @@ describe(path.relative(process.cwd(), import.meta.filename), () => {
 
     describe("add attachments", () => {
       for (const [version, endpoint] of [
-        ["v1", XRAY_CLIENT_CLOUD.attachment.addAttachment.v1],
-        ["v2", XRAY_CLIENT_CLOUD.attachment.addAttachment],
+        ["v1", XRAY_CLIENT_CLOUD.attachment.v1],
+        ["v2", XRAY_CLIENT_CLOUD.attachment],
       ] as const) {
         describe(version, () => {
-          it(`adds attachments (${version})`, async () => {
-            const content = await endpoint(join("test", "resources", "mini.txt"));
+          it(`adds attachments (${version})`, async (context) => {
+            context.mock.method(XRAY_CLIENT_CLOUD.attachment, "addAttachment");
+            const content = await endpoint.addAttachment(join("test", "resources", "mini.txt"));
             assert.ok(content.created);
             assert.ok(content.id);
             assert.strictEqual(content.filename, "mini.txt");
