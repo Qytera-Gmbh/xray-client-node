d7861c580838182d5d8b0fdc07c6c5a27ca275a8
Sebastian Vollbrecht <sebivo@freenet.de>
2025-03-02 17:26:58 +0100

    Improve version handling in import execution endpoint

--- src/endpoints/cloud/import/execution/import-execution.spec.ts
+++ src/endpoints/cloud/import/execution/import-execution.spec.ts
@@ -8,12 +8,12 @@ import { DATA_CLOUD } from "../../../../../test/test-data-cloud.js";
 describe(path.relative(process.cwd(), import.meta.filename), () => {
   describe("xray", () => {
     for (const [version, endpoint, issue] of [
-      ["v1", XRAY_CLIENT_CLOUD.import.execution.xray.v1, DATA_CLOUD.testExecutions.importXray.v1],
-      ["v2", XRAY_CLIENT_CLOUD.import.execution.xray, DATA_CLOUD.testExecutions.importXray.v2],
+      ["v1", XRAY_CLIENT_CLOUD.import.execution.v1, DATA_CLOUD.testExecutions.importXray.v1],
+      ["v2", XRAY_CLIENT_CLOUD.import.execution, DATA_CLOUD.testExecutions.importXray.v2],
     ] as const) {
       describe(version, () => {
         it("imports xray results in xray cloud", async () => {
-          const data = await endpoint({
+          const data = await endpoint.xray({
             testExecutionKey: issue.key,
             tests: [{ status: "PASSED", testKey: issue.tests[0].key }],
           });
@@ -27,19 +27,15 @@ describe(path.relative(process.cwd(), import.meta.filename), () => {
     for (const [version, endpoint, issue] of [
       [
         "v1",
-        XRAY_CLIENT_CLOUD.import.execution.xrayMultipart.v1,
+        XRAY_CLIENT_CLOUD.import.execution.v1,
         DATA_CLOUD.testExecutions.importXrayMultipart.v1,
       ],
-      [
-        "v2",
-        XRAY_CLIENT_CLOUD.import.execution.xrayMultipart,
-        DATA_CLOUD.testExecutions.importXrayMultipart.v2,
-      ],
+      ["v2", XRAY_CLIENT_CLOUD.import.execution, DATA_CLOUD.testExecutions.importXrayMultipart.v2],
     ] as const) {
       describe(version, () => {
         it("imports xray multipart results in xray cloud", async () => {
           const description = randomUUID();
-          const data = await endpoint(
+          const data = await endpoint.xrayMultipart(
             {
               testExecutionKey: issue.key,
               tests: [
