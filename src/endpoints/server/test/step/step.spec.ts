ff9fb2bc946de5bb4e0d9c2ced94ffeeb2740922
Sebastian Vollbrecht <sebivo@freenet.de>
2025-03-16 21:48:28 +0100

    Add `createStep` server endpoints

--- src/endpoints/server/test/step/step.spec.ts
+++ src/endpoints/server/test/step/step.spec.ts
@@ -1,4 +1,5 @@
 import assert from "node:assert";
+import { randomUUID } from "node:crypto";
 import path from "node:path";
 import { describe, it } from "node:test";
 import { XRAY_CLIENT_SERVER } from "../../../../../test/clients.js";
@@ -78,5 +79,70 @@ describe(path.relative(process.cwd(), import.meta.filename), () => {
         });
       });
     }
+  });
+
+  describe("createStep", () => {
+    describe("v1", () => {
+      it("creates steps", async () => {
+        const uuid = randomUUID();
+        const content = await XRAY_CLIENT_SERVER.test.step.v1.createStep(
+          DATA_SERVER.tests.addTestSteps.v1.key,
+          { data: `data-${uuid}`, result: `result-${uuid}`, step: `step-${uuid}` }
+        );
+        assert.strictEqual(typeof content.id, "number");
+        const steps = await XRAY_CLIENT_SERVER.test.step.v1.getSteps(
+          DATA_SERVER.tests.addTestSteps.v1.key
+        );
+        const lastStep = steps[steps.length - 1];
+        assert.deepStrictEqual(lastStep.step, {
+          raw: `step-${uuid}`,
+          rendered: `<p>step-${uuid}</p>`,
+        });
+        assert.deepStrictEqual(lastStep.data, {
+          raw: `data-${uuid}`,
+          rendered: `<p>data-${uuid}</p>`,
+        });
+        assert.deepStrictEqual(lastStep.result, {
+          raw: `result-${uuid}`,
+          rendered: `<p>result-${uuid}</p>`,
+        });
+      });
+    });
+
+    describe("v2", () => {
+      it("creates steps", async () => {
+        const uuid = randomUUID();
+        const content = await XRAY_CLIENT_SERVER.test.step.createStep(
+          DATA_SERVER.tests.addTestSteps.v2.key,
+          {
+            fields: {
+              ["Action"]: `step-${uuid}`,
+              ["Data"]: `data-${uuid}`,
+              ["Expected Result"]: `result-${uuid}`,
+            },
+          }
+        );
+        assert.strictEqual(typeof content.step.id, "number");
+        const steps = await XRAY_CLIENT_SERVER.test.step.getSteps(
+          DATA_SERVER.tests.addTestSteps.v2.key
+        );
+        const lastStep = steps.steps[steps.steps.length - 1];
+        assert.ok("fields" in lastStep);
+        assert.deepStrictEqual(lastStep.fields, {
+          ["Action"]: {
+            type: "Wiki",
+            value: { raw: `step-${uuid}`, rendered: `<p>step-${uuid}</p>` },
+          },
+          ["Data"]: {
+            type: "Wiki",
+            value: { raw: `data-${uuid}`, rendered: `<p>data-${uuid}</p>` },
+          },
+          ["Expected Result"]: {
+            type: "Wiki",
+            value: { raw: `result-${uuid}`, rendered: `<p>result-${uuid}</p>` },
+          },
+        });
+      });
+    });
   });
 });
--- src/endpoints/server/test/step/step.ts
+++ src/endpoints/server/test/step/step.ts
@@ -15,6 +15,7 @@ interface GetSteps {
    * value).
    *
    * @param testKey the key of the test issue
+   * @param query the query parameters
    * @returns the test step details
    *
    * @see https://docs.getxray.app/display/XRAY/v2.0#/Test%20Step/get_test__testKey__steps
@@ -103,13 +104,136 @@ interface GetAttachments {
   };
 }
 
+interface CreateStep {
+  /**
+   * Create a new test step. All step fields values are listed under fields and identified by the
+   * name and a list of attachments to add to the step. The field values should follow a certain
+   * format according to the custom field type.
+   *
+   * - **Toggle Fields** values should be `"true"`, `"false"`, `"0"` or `"1"`
+   * - **Number Fields** values should be a string containing a number, e.g. `"320"`. Decimal number
+   * are also accepted, for instance, `"320.5"`. The decimal places are always separated by a `.`
+   * - **Single Select and Radio Button Fields** values should be a single string containing the
+   * option value, e.g. `"Option A"`. The value should be a valid option for that custom field.
+   * The values are not case sensitive.
+   * - **Multiple Select Fields** values should be an array of the string of option values, e.g.
+   * `["Option A", "Option B"]`. All the selected values should be valid for that custom field. The
+   * option values are not case sensitive.
+   * - **Date Fields** should follow the ISO format _yyyy-MM-dd_, where _yyyy_ represents the year,
+   * _MM_ the month in year and _dd_ the day in the month. For instance, a valid value would be
+   * `"2020-05-02"`.
+   * - **Date Time fields** should be in UTC following the ISO format _yyyy-MM-dd'T'HH:mm'Z'_.
+   * The date part follows the same date format, while _HH_ represents the hours (0-24), _mm_ the
+   * minutes in hour and _Z_ indicates the hour in UTC. For instance, a valid date time value would
+   * be `"2020-05-02T10:30Z"`.
+   *
+   * The remaining field types values do not required a specific validation. For instance, for a
+   * single line custom field a valid value would be `"Perform Action B"`.
+   *
+   * If some value is not valid for a certain custom field type, an error will be return stating the
+   * field with the incorrect value. Furthermore, all required step fields must be provided.
+   *
+   * @param testKey the key of the test issue
+   * @param query the query parameters
+   * @param body the new step
+   * @returns the step creation result
+   *
+   */
+  createStep(
+    testKey: string,
+    body: {
+      attachments?: File[];
+      /**
+       * @example
+       *
+       * ```ts
+       * {
+       *   Action: "Step Action",
+       *   Date: "01/Feb/2022",
+       *   Toggle: "true",
+       *   SingleSelectList: "Selected Value",
+       *   MultiSelectList: ["Option A", "Option B"]
+       * }
+       * ```
+       */
+      fields: Record<string, string | string[]>;
+    },
+    query?: {
+      /**
+       * A test version.
+       *
+       * @example "manual_2"
+       */
+      testVersion?: string;
+    }
+  ): Promise<CreateStepResponse>;
+  v1: {
+    /**
+     * Create a new test step.
+     *
+     * @param testKey the key of the test issue
+     * @param body the new step
+     * @returns the step creation result
+     *
+     * @see https://docs.getxray.app/display/XRAY/Test+Steps+-+REST
+     */
+    createStep(
+      testKey: string,
+      body: {
+        attachments?: File[];
+        /**
+         * @example "example data"
+         */
+        data?: string;
+        /**
+         * @example "example result"
+         */
+        result?: string;
+        /**
+         * @example "example step"
+         */
+        step: string;
+      }
+    ): Promise<CreateStepResponse["step"]>;
+  };
+}
+
+interface File {
+  /**
+   * @example "plain/text"
+   */
+  contentType: string;
+  /**
+   * @example "aGVsbG8gd29ybGQ="
+   */
+  data: string;
+  /**
+   * @example "example.txt"
+   */
+  filename: string;
+}
+
+interface CreateStepResponse {
+  step: {
+    /**
+     * @example [25737, 25738]
+     */
+    attachmentIds: number[];
+    /**
+     * @example 85860
+     */
+    id: number;
+  };
+  warnings: string[];
+}
+
 /**
  * Models the test step endpoints in Xray server.
  *
  * @see https://docs.getxray.app/display/XRAY/Test+Steps+-+REST
  * @see https://docs.getxray.app/display/XRAY/v2.0#/Test%20Step
  */
-export class TestStepApi extends BaseApi implements GetSteps, GetStep, GetAttachments {
+export class TestStepApi extends BaseApi implements GetSteps, GetStep, GetAttachments, CreateStep {
   private readonly processor = {
     getAttachments: async (url: string) => {
       const response = await this.client.send(url, {
@@ -120,30 +244,53 @@ export class TestStepApi extends BaseApi implements GetSteps, GetStep, GetAttach
     },
   };
 
-  public readonly v1: GetSteps["v1"] & GetStep["v1"] & GetAttachments["v1"] = this.bind((self) => ({
-    async getAttachments(testKey, id) {
-      return self.processor.getAttachments(
-        `rest/raven/1.0/api/test/${testKey}/step/${id.toString()}/attachment`
-      );
-    },
-    async getStep(testKey, id) {
-      const response = await self.client.send(
-        `rest/raven/1.0/api/test/${testKey}/step/${id.toString()}`,
-        {
+  public readonly v1: GetSteps["v1"] & GetStep["v1"] & GetAttachments["v1"] & CreateStep["v1"] =
+    this.bind((self) => ({
+      async createStep(testKey, body) {
+        const response = await self.client.send(`rest/raven/1.0/api/test/${testKey}/step`, {
+          body: JSON.stringify(body),
+          expectedStatus: 200,
+          headers: { ["Content-Type"]: "application/json" },
+          method: "PUT",
+        });
+        return (await response.json()) as Awaited<ReturnType<CreateStep["v1"]["createStep"]>>;
+      },
+      async getAttachments(testKey, id) {
+        return self.processor.getAttachments(
+          `rest/raven/1.0/api/test/${testKey}/step/${id.toString()}/attachment`
+        );
+      },
+      async getStep(testKey, id) {
+        const response = await self.client.send(
+          `rest/raven/1.0/api/test/${testKey}/step/${id.toString()}`,
+          {
+            expectedStatus: 200,
+            method: "GET",
+          }
+        );
+        return (await response.json()) as Awaited<ReturnType<GetStep["v1"]["getStep"]>>;
+      },
+      async getSteps(testKey) {
+        const response = await self.client.send(`rest/raven/1.0/api/test/${testKey}/step`, {
           expectedStatus: 200,
           method: "GET",
-        }
-      );
-      return (await response.json()) as Xray.Test.Step.DetailsV1;
-    },
-    async getSteps(testKey) {
-      const response = await self.client.send(`rest/raven/1.0/api/test/${testKey}/step`, {
-        expectedStatus: 200,
-        method: "GET",
-      });
-      return (await response.json()) as Xray.Test.Step.DetailsV1[];
-    },
-  }));
+        });
+        return (await response.json()) as Awaited<ReturnType<GetSteps["v1"]["getSteps"]>>;
+      },
+    }));
+
+  public async createStep(
+    ...[testKey, body, query]: Parameters<CreateStep["createStep"]>
+  ): ReturnType<CreateStep["createStep"]> {
+    const response = await this.client.send(`rest/raven/2.0/api/test/${testKey}/steps`, {
+      body: JSON.stringify(body),
+      expectedStatus: 201,
+      headers: { ["Content-Type"]: "application/json" },
+      method: "POST",
+      query,
+    });
+    return (await response.json()) as Awaited<ReturnType<CreateStep["createStep"]>>;
+  }
 
   public async getAttachments(
     ...[testKey, id]: Parameters<GetAttachments["getAttachments"]>
